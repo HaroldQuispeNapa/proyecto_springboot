e50bc8d97d41bdb63720bd5656bfc1cee455d1f1
HaroldQn <>
2025-10-20 15:16:51 -0500

    Creando los paquetes de controlador, modelo y servicio

---
+++ src/main/java/com/example/proys11/controlador/eliminame.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.proys11.controlador;
+
+/**
+ *
+ * @author LAB-USR-LNORTE
+ */
+public class eliminame {
+    
+    
+}
---
+++ src/main/java/com/example/proys11/interfaces/eliminame.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.proys11.interfaces;
+
+/**
+ *
+ * @author LAB-USR-LNORTE
+ */
+public class eliminame {
+    
+}
