936e0b842f6ff5b6ba5d9b28deb53849c047fce5
HaroldQn <>
2025-10-20 14:50:31 -0500

    Inicializando proyecto

---
+++ src/main/java/com/example/proys11/Proys11Application.java
@@ -0,0 +1,13 @@
+package com.example.proys11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proys11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proys11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proys11/Proys11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proys11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proys11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
