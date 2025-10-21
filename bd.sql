a5eda53926ea279ece3736beba46c60aab7f803d
HaroldQn <>
2025-10-21 15:28:09 -0500

    bd

---
+++ bd.sql
@@ -0,0 +1,58 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  LAB-USR-LNORTE
+ * Created: 21 oct 2025
+ */
+
+CREATE DATABASE bddnegocio;
+use bddnegocio;
+
+create TABLE usuarios(
+	idusuario int primary key auto_increment,
+	usuario varchar(50),
+	clave varchar(60),
+    nombre varchar(50)
+)ENGINE=INNODB;
+
+insert into usuarios (usuario, clave,nombre) values('hquispe','123456','Harold Quispe');
+update usuarios set clave = "$2a$10$MNTtqGBlSGX2zkAFzW7WLeDV/A0lVUyNGqwoKealHKd47j96KKKce" where idusuario = 1;
+select * from usuarios;
+
+CREATE TABLE categorias(
+	idcategoria INT primary key auto_increment,
+    nombre varchar(45) not null unique
+)ENGINE = INNODB;
+
+INSERT INTO categorias(nombre) values("Cemento"),("Fierro"),("Agregados");
+select * from categorias;
+
+CREATE TABLE material(
+idmaterial int primary key auto_increment,
+nombre varchar(45) NOT NULL unique,
+cantidad decimal(10,2) not null,
+idcategoria int,
+constraint fk_idcategoria foreign key (idcategoria) references categorias(idcategoria)
+)ENGINE = INNODB;
+INSERT INTO material(nombre,cantidad,idcategoria)values("SOL",10,1),("ACEROS AREQUIPA",50,2),("INKA",5,1),("LADRILLO",1000,3);
+
+select 
+	m.idmaterial,
+    m.nombre,
+    m.cantidad,
+    c.idcategoria,
+	c.nombre as 'categoria'
+ from material m inner join categorias c on m.idcategoria = c.idcategoria order by idmaterial;
+ 
+ Delete from material where idcategoria = 1;
+ 
+CREATE TABLE Colaborador(
+idcolaborador INT PRIMARY KEY AUTO_INCREMENT,
+nombre VARCHAR(45) NOT NULL,
+correo VARCHAR(45) NOT NULL UNIQUE,
+telefono VARCHAR(45) NOT NULL
+)ENGINE = INNODB;
+
+select * from Colaborador;
